2fd08a75672fabf8adf3cdc49b31da4cb54e74d7
Brett Bedevian <brettbedevian@gmail.com>
2020-02-18 16:19:59 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,10 +9,12 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-  movie_index = 0
+  name_index = 0
   title_index = 0
-  while movie_index < directors_database.length
-  if directors_database[movie_index] == "Stephen Speilberg"
+  while name_index < directors_database.length
+  if directors_database[name_index] == "Stephen Speilberg"
 return directors_database[movie_index][:movies][title_index][:title]
-index +=1
+
+name_ index +=1
+end 
 end
